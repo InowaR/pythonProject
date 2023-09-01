db152b5839f70639c04716dad31ac72967206cde
georgiy <georgii_sidenko@mail.ru>
2023-09-01 15:16:35 +0300

    fix

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', include('women.urls')),
+    path('', include('women.urls')),
 ]
--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),  # http://localhost:8000/women/
-    path('cats/', categories),  # http://localhost:8000/women/cats/
+    path('', index),  # http://localhost:8000/
+    path('cats/', categories),  # http://localhost:8000/cats/
 ]
