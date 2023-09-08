bd07774ac5f0cc731c89ee90f450f79fa52097a1
georgiy <georgii_sidenko@mail.ru>
2023-09-08 22:47:02 +0300

    added templates and base.html

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),  # http://localhost:8000/
+    path('about/', about, name='about'),
     path('cats/<int:catid>/', categories),  # http://localhost:8000/cats/1/
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
